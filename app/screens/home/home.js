b75406d45453c7a1b37e2ce25e37eb63e0982cbf
yumei.shih <yumeishih17@gmail.com>
2019-03-22 15:03:10 +0800

    add note title to home page

--- app/screens/home/home.js
+++ app/screens/home/home.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, TextInput } from 'react-native'
 import TextArea from 'Components/textArea'
 import styles from './style'
 
-const Home = () => (
-  <View style={styles.container}>
-    <Text> Please enter your note here</Text>
-    <TextArea />
-  </View>
-)
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+    }
+  }
+
+  setTitle = title => this.setState({ title })
+
+  render() {
+    const { title } = this.state
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleHeading}> Note Title</Text>
+        <TextInput
+          style={styles.titleTextInput}
+          onChangeText={this.setTitle}
+          value={title}
+        />
+        <Text style={styles.textAreaTitle}> Please type your note below </Text>
+        <TextArea style={styles.textArea} />
+      </View>
+    )
+  }
+}
 
 export default Home
