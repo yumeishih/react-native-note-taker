6f5cede991e0c914886e749ddd4cbefef098876a
yumei.shih <yumeishih17@gmail.com>
2019-03-22 12:19:41 +0800

    restructure textArea

--- app/components/textArea/index.js
+++ app/components/textArea/index.js
@@ -1,34 +1,3 @@
-import React, { Component } from 'react'
-import { TextInput } from 'react-native'
-import styles from './style'
+import TextArea from './textArea'
 
-class TextArea extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      text: '',
-    }
-  }
-
-  handleTextChange = (text) => {
-    this.setState({
-      text,
-    })
-  }
-
-  render() {
-    const { ...extraProps } = this.props
-    const { text } = this.state
-    return (
-      <TextInput
-        {...extraProps}
-        style={[styles.textArea, extraProps.style]}
-        multiline
-        onChangeText={this.handleTextChange}
-        value={text}
-      />
-    );
-  }
-}
-
-export default TextArea;
+export default TextArea
---
+++ app/components/textArea/textArea.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { TextInput } from 'react-native'
+import styles from './style'
+
+class TextArea extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: '',
+    }
+  }
+
+  handleTextChange = (text) => {
+    this.setState({
+      text,
+    })
+  }
+
+  render() {
+    const { ...extraProps } = this.props
+    const { text } = this.state
+    return (
+      <TextInput
+        {...extraProps}
+        style={[styles.textArea, extraProps.style]}
+        multiline
+        onChangeText={this.handleTextChange}
+        value={text}
+      />
+    )
+  }
+}
+
+export default TextArea
